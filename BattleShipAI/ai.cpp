880d11abd0683535abb256035d49557771e21b93
DemonPenguin325 <demonpenguin325@gmail.com>
2019-01-23 21:20:40 -0700

    Week 1 changes

--- BattleShipAI/ai.cpp
+++ BattleShipAI/ai.cpp
@@ -10,15 +10,51 @@ void ComputerPlayer::turn(Player &target)
     static int lastx = 0;
     static int lasty = 0;
     static int hitstreak = 0;
-    
+    static bool prevHit = false;
+
     int x,y;
     
     do
     {
         // AI code goes here
+        if (prevHit == false)
+        {
         x = rand() % BOARDX; // Pick a random x
         y = rand() % BOARDY; // Pick a random y
-        
+        }
+
+        else
+        {
+            int direction = rand() % 4;
+            switch (direction)
+            {
+                case 0: // south
+                    x = lastx;
+                    y = lasty + 1;
+                    break;
+                case 1: //east
+                    x = lastx + 1;
+                    y = lasty;
+                    break;
+                case 2: // north
+                    x = lastx;
+                    y + lasty - 1;
+                    break;
+                case 3: // west
+                    x = lastx - 1;
+                    y = lasty;
+                    break;
+            }
+            if (x < 0 || x >= BOARDX || y < 0 || y >= BOARDY)
+                continue;
+            
+        }
+        if (target.map[x][y]==SHIP)
+        {
+            lastx = x;
+            lasty = y;
+            prevHit = true;
+        }        
         // End AI code
         // Message the user with the results of your shot
         if (target.map[x][y] == HIT || target.map[x][y] == MISS) {continue;}
