cd6f76b948fc24520fb057ad004bd24bc074fce9
DemonPenguin325 <demonpenguin325@gmail.com>
2019-01-25 20:51:08 -0700

    Week 02 Progress

--- BattleShipAI/ai.cpp
+++ BattleShipAI/ai.cpp
@@ -7,25 +7,36 @@ using namespace std;
 
 void ComputerPlayer::turn(Player &target)
 {
+  enum STATE{RAND, CHECK, FOLLOW};
+  
+    static STATE state = RAND;
+  
     static int lastx = 0;
     static int lasty = 0;
     static int hitstreak = 0;
-    static bool prevHit = false;
-
+    
     int x,y;
+    
+    static int direction = -1;
     
     do
     {
         // AI code goes here
-        if (prevHit == false)
-        {
+      switch (state)
+	{
+	case RAND:
         x = rand() % BOARDX; // Pick a random x
         y = rand() % BOARDY; // Pick a random y
-        }
-
-        else
+        if (target.map[x][y]==SHIP)
         {
-            int direction = rand() % 4;
+            lastx = x;
+            lasty = y;
+            state = CHECK;
+        }
+	  break;
+	  
+	case CHECK:
+        direction = rand() % 4;
             switch (direction)
             {
                 case 0: // south
@@ -45,16 +56,43 @@ void ComputerPlayer::turn(Player &target)
                     y = lasty;
                     break;
             }
-            if (x < 0 || x >= BOARDX || y < 0 || y >= BOARDY)
+	     if (target.map[x][y]==SHIP)
+	       state = FOLLOW;
+	  break;
+	  
+	case FOLLOW:
+	   switch (direction)
+            {
+                case 0: // south
+                    x = lastx;
+                    y = ++lasty; 
+                    break;
+                case 1: //east
+		  x = ++lastx;
+                    y = lasty;
+                    break;
+                case 2: // north
+                    x = lastx;
+                    y = --lasty;
+                    break;
+                case 3: // west
+		  x = --lastx;
+                    y = lasty;
+                    break;
+            }
+	   if (target.map[x][y]==BLANK)
+	     state = RAND;  
+	  break;
+
+	}
+      
+        
+
+                   if (x < 0 || x >= BOARDX || y < 0 || y >= BOARDY)
                 continue;
             
-        }
-        if (target.map[x][y]==SHIP)
-        {
-            lastx = x;
-            lasty = y;
-            prevHit = true;
-        }        
+
+               
         // End AI code
         // Message the user with the results of your shot
         if (target.map[x][y] == HIT || target.map[x][y] == MISS) {continue;}
