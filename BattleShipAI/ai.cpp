c25d1aac2316fd1ea356229675da8b66f8a50d65
DemonPenguin325 <demonpenguin325@gmail.com>
2019-01-31 17:47:30 -0700

    Added additional validity checks
    Also fixed spacing and added a new catch debug message

--- BattleShipAI/ai.cpp
+++ BattleShipAI/ai.cpp
@@ -23,92 +23,96 @@ void ComputerPlayer::turn(Player &target)
     {
         // AI code goes here
       switch (state)
-	{
-	case RAND:
-        x = rand() % BOARDX; // Pick a random x
-        y = rand() % BOARDY; // Pick a random y
-        if (target.map[x][y]==SHIP)
         {
-            lastx = x;
-            lasty = y;
-            state = CHECK;
+        case RAND:
+            x = rand() % BOARDX; // Pick a random x
+            y = rand() % BOARDY; // Pick a random y
+            if (target.map[x][y]==SHIP)
+            {
+                lastx = x;
+                lasty = y;
+                state = CHECK;
+            }
+          break;
+          
+        case CHECK:
+            direction = rand() % 4;
+                switch (direction)
+                {
+                    case 0: // south
+                        x = lastx;
+                        y = lasty + 1;
+                        break;
+                    case 1: //east
+                        x = lastx + 1;
+                        y = lasty;
+                        break;
+                    case 2: // north
+                        x = lastx;
+                        y = lasty - 1;
+                        break;
+                    case 3: // west
+                        x = lastx - 1;
+                        y = lasty;
+                        break;
+                }
+             if (target.map[x][y]==SHIP)
+               state = FOLLOW;
+          break;
+          
+        case FOLLOW:
+           switch (direction)
+                {
+                    case 0: // south
+                        x = lastx;
+                        y = ++lasty;
+                        break;
+                    case 1: //east
+                        x = ++lastx;
+                        y = lasty;
+                        break;
+                    case 2: // north
+                        x = lastx;
+                        y = --lasty;
+                        break;
+                    case 3: // west
+                        x = --lastx;
+                        y = lasty;
+                        break;
+                }
+                // This is so we don't keep going if the next spot in the grid is already marked
+                if (target.map[x][y]==MISS || target.map[x][y]==HIT){
+                    state = RAND;
+                    continue;
+                }
+           if (target.map[x][y]==BLANK)
+             state = RAND;
+          break;
         }
-	  break;
-	  
-	case CHECK:
-        direction = rand() % 4;
-            switch (direction)
-            {
-                case 0: // south
-                    x = lastx;
-                    y = lasty + 1;
-                    break;
-                case 1: //east
-                    x = lastx + 1;
-                    y = lasty;
-                    break;
-                case 2: // north
-                    x = lastx;
-                    y + lasty - 1;
-                    break;
-                case 3: // west
-                    x = lastx - 1;
-                    y = lasty;
-                    break;
-            }
-	     if (target.map[x][y]==SHIP)
-	       state = FOLLOW;
-	  break;
-	  
-	case FOLLOW:
-	   switch (direction)
-            {
-                case 0: // south
-                    x = lastx;
-                    y = ++lasty; 
-                    break;
-                case 1: //east
-		  x = ++lastx;
-                    y = lasty;
-                    break;
-                case 2: // north
-                    x = lastx;
-                    y = --lasty;
-                    break;
-                case 3: // west
-		  x = --lastx;
-                    y = lasty;
-                    break;
-            }
-	   if (target.map[x][y]==BLANK)
-	     state = RAND;  
-	  break;
-
-	}
-      
         
-
-                   if (x < 0 || x >= BOARDX || y < 0 || y >= BOARDY)
-                continue;
-            
-
-               
+        if (x < 0 || x >= BOARDX || y < 0 || y >= BOARDY)
+            continue;
+        
         // End AI code
+        
         // Message the user with the results of your shot
         if (target.map[x][y] == HIT || target.map[x][y] == MISS) {continue;}
-        if (target.map[x][y] == SHIP)
+        else if (target.map[x][y] == SHIP)
         {
-            cout<<"\nThe computer hit your battleship!\n";
+            cout<<"\nThe computer hit your battleship! ("<<x+1<<','<<y+1<<")\n";
             target.map[x][y] = HIT;
             hit_map[x][y] = HIT;
             break;
         }
-        if (target.map[x][y] == BLANK)
+        else if (target.map[x][y] == BLANK)
         {
-            cout<<"\nThe computer missed!\n";
+            cout<<"\nThe computer missed! ("<<x+1<<','<<y+1<<")\n";
             target.map[x][y] = MISS;
             hit_map[x][y] = MISS;
             break;
+        }
+        else{
+            cout<<"\nSomething went very wrong with the AI targeting system.\n";
         }
     }while (target.map[x][y] != BLANK);
 }
